46e8df2ef33a42456c2bb50f55849026b2e78551
david.horwitz@uct.ac.za <david.horwitz@uct.ac.za@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-05-08 13:27:43 +0000

    SAK-13499 register the bean
    git-svn-id: https://source.sakaiproject.org/svn/profile/branches/SAK-13499@46293 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- common-composite-component/src/java/org/sakaiproject/component/common/edu/person/SakaiPersonEntityProviderImpl.java
+++ common-composite-component/src/java/org/sakaiproject/component/common/edu/person/SakaiPersonEntityProviderImpl.java
@@ -1,7 +1,5 @@
 package org.sakaiproject.component.common.edu.person;
 
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.api.common.edu.person.SakaiPerson;
